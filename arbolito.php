54fa6703e4b1c79b33c7662c3daf1f2bc7fe36e8
Cjlopez <148722482+carlos55859@users.noreply.github.com>
2024-03-31 17:58:39 -0500

    Update arbolito.php

--- arbolito.php
+++ arbolito.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Arbolito</title>
+    <title>Arbolito de navidad </title>
     <link rel="stylesheet" href="./css/stylos.css">
 </head>
 
